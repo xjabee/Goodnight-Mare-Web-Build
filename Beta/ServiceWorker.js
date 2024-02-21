d2cda5620fd0c577fac1c84efd1213319e223a77
xJabee <jabee.sevilla@gmail.com>
2024-02-22 00:54:58 +0800

    Added Beta Page

---
+++ Beta/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Irregular Devs-GoodNight, Mare-1";
+const contentToCache = [
+    "Build/Beta.loader.js",
+    "Build/Beta.framework.js",
+    "Build/Beta.data",
+    "Build/Beta.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
